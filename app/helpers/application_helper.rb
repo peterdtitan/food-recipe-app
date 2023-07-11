17f72dd7bb4fb2e2c69f222a0d9483e903dc3a35
Peter Okorafor <peterdtitan@gmail.com>
2023-07-11 16:13:11 +0300

    Added a Navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,2 @@
 module ApplicationHelper
-  def navigation
-    content_tag(:header, class: 'navigation') do
-      content_tag(:nav) do
-        link_to('Home', root_path) +
-          link_to('Login', new_user_session_path)
-      end
-    end +
-      content_tag(:div, class: 'notice alert') do
-        content_tag(:p, notice) +
-          content_tag(:p, alert)
-      end
-  end
 end
